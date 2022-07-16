f90692cafc35bb280f5617532ceb11a725a576be
wspromise <3218356596@qq.com>
2022-07-16 16:42:38 +0800

    doc:总结vue初始化流程并添加注释

--- 07.模板字符串生成/dist/vue.js
+++ 07.模板字符串生成/dist/vue.js
@@ -345,9 +345,10 @@
     let ast = parserHTML(template);
     console.log(ast); // 2.代码生成
 
-    let code = generate(ast);
+    let code = generate(ast); // 模板引擎的实现原理都是new Function + with  (ejs, jade等模板引擎都是这样实现的)
+
     let render = new Function(`with(this){return ${code}}`);
-    console.log(render.toString(), 'render'); // 1.编译原理
+    return render; // 1.编译原理
     // 2.响应式原理 依赖收集
     // 3.组件化开发（贯穿了vue的流程）
     // 4.diff算法
